2acf24993a21ddbb31d5fd69be4dacb9579d1f13
Cole Henke <cole.henke@ndus.edu>
2024-04-01 21:24:59 -0500

    Add code to count number of times site has been visited

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -7,8 +7,10 @@ def index(request):
     num_instances = BookInstance.objects.all().count()
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count()
-
     num_genres = Genre.objects.all().count()
+
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
 
     context = {
         'num_books': num_books,
@@ -16,9 +18,9 @@ def index(request):
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
         'num_genres': num_genres,
+        'num_visits': num_visits,
     }
 
-    # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
 
 class BookListView(generic.ListView):
