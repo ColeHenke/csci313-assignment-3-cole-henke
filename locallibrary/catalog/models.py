72707e4dbfaaa3ca42a185ab0ff79d6b8fc05501
Cole Henke <cole.henke@ndus.edu>
2024-04-02 00:21:03 -0500

    Implement authentication and permissions

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -3,6 +3,10 @@ from django.urls import reverse
 from django.db.models import UniqueConstraint
 from django.db.models.functions import Lower
 import uuid
+from django.conf import settings
+from datetime import date
+
+
 
 class Genre(models.Model):
     """Model representing a book genre."""
@@ -63,13 +67,13 @@ class Book(models.Model):
     display_genre.short_description = 'Genre'
 
 class BookInstance(models.Model):
-
     """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
     id = models.UUIDField(primary_key=True, default=uuid.uuid4,
                           help_text="Unique ID for this particular book across whole library")
     book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True)
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
@@ -88,6 +92,12 @@ class BookInstance(models.Model):
 
     class Meta:
         ordering = ['due_back']
+        permissions = (("can_mark_returned", "Set book as returned"),)
+
+    @property
+    def is_overdue(self):
+        """Determines if the book is overdue based on due date and current date."""
+        return bool(self.due_back and date.today() > self.due_back)
 
     def __str__(self):
         """String for representing the Model object."""
